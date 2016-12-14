02464ea80b973264c69c4f495906eec0355b3a53
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-14 15:28:09 -0800

    spaces

--- R/schema_validate.R
+++ R/schema_validate.R
@@ -13,17 +13,17 @@
 #' x <- system.file("examples", "example_bad1.xml", package = "finch")
 #' # schema_validate(x)
 #'
-#'schema_path <- system.file("schemas/tdwg_dwcterms.xsd", package = "finch")
+#' schema_path <- system.file("schemas/tdwg_dwcterms.xsd", package = "finch")
 #'
-#'# simple dwc file
-#'file_path <- system.file("examples/example_simple.xml", package = "finch")
-#'schema_path <- system.file("schemas/tdwg_dwc_simple.xsd", package = "finch")
-#'schema_validate(file, schema_path)
+#' # simple dwc file
+#' file_path <- system.file("examples/example_simple.xml", package = "finch")
+#' schema_path <- system.file("schemas/tdwg_dwc_simple.xsd", package = "finch")
+#' schema_validate(file, schema_path)
 #'
-#'# classes dwc file
-#'file_path <- system.file("examples", "example_classes_specimen.xml", package = "finch")
-#'schema_path <- system.file("schemas/tdwg_dwc_class_terms.xsd", package = "finch")
-#'schema_validate(file, schema_path)
+#' # classes dwc file
+#' file_path <- system.file("examples", "example_classes_specimen.xml", package = "finch")
+#' schema_path <- system.file("schemas/tdwg_dwc_class_terms.xsd", package = "finch")
+#' schema_validate(file, schema_path)
 #' }
 schema_validate <- function(file_path, schema_path) {
   #schema <- system.file("dca_schema.xml", package = "finch")
